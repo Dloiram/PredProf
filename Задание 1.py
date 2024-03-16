28c8ba0f19439f5506ea5f0ac4377762008617fc
Dloiram <125088358+Dloiram@users.noreply.github.com>
2024-03-16 17:44:53 +0300

    Update Задание 1.py

--- Задание 1.py
+++ Задание 1.py
@@ -1,10 +1,11 @@
-def products():
+def products(): 
     a = open("products.txt", "r+", encoding='utf-8').readlines()
     for i in range(len(a)):
         a[i] = list(map(str,a[i].split(";")))
         a[i][-1] = a[i][-1][:-1]
     return a
-
+    
+""" Функция считывания Файла """
 
 a = products()
 
@@ -14,6 +15,7 @@ c = 0
 for i in range(len(a)):
     if a[i][0] == "Закуски":
         c += float(a[i][-1]) * float(a[i][-2])
+"""Тело кода"""
 
 
 print(c)
